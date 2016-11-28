81ba88f7cb9b8b99830874afd763eaa17818599e
Joni Korpi <me@jonikorpi.com>
2016-11-28 18:53:58 +0200

    Spawning via async/await

--- queue.js
+++ queue.js
@@ -21,70 +21,145 @@ process.on("SIGINT", function() {
   });
 });
 
-// Player queue
-var playerQueue = new Queue(database.ref("playerQueue"), {"numWorkers": 5}, function(data, progress, resolve, reject) {
-  var request = data.request;
-  console.log("playerQueue:", request.action, "for", request.playerID);
+// Functions
 
-  if (request) {
-    var updates = {};
-
-    switch (request.action) {
-
-      case "spawn":
-        database.ref("playerSecrets/" + request.playerID).once("value", function(snapshot) {
-          var player = snapshot.val();
-
-          if (player && player.location && player.location.x && player.location.y) {
-            reject("Player has already spawned");
-          }
-
-          var spawnLocation = [0,0];
-
-          updates["playerSecrets/" + request.playerID] = {
-            location: {
-              x: spawnLocation[0],
-              y: spawnLocation[1],
-            },
-          };
-
-          updates[`locations/${spawnLocation[0]}/${spawnLocation[1]}/object`] = {
-            type: "player",
-            playerID: request.playerID,
-            previousLocation: {
-              x: spawnLocation[0],
-              y: spawnLocation[1],
-            },
-          }
-
-          database.ref().update(updates).then(resolve).catch(reject);
-        }).catch(reject);
-        break;
-
-      default:
-        reject("Unknown action");
-
+var prepareRequest = function(data, progress, resolve, reject) {
+  try {
+    if (data.request) {
+      console.log("Processing", data.request.action, "for", data.request.playerID);
+      processRequest(data.request, progress, resolve, reject);
+    }
+    else {
+      console.log("Rejecting", data.request.action, "for", data.request.playerID);
+      reject("Missing request");
     }
   }
-  else {
-    reject("Missing request or request attributes");
+  catch (error) {
+    console.log(error);
   }
-});
+}
+
+var processRequest = async function(request, progress, resolve, reject) {
+  var updates = {};
+
+  switch (request.action) {
+
+    // Spawn
+    case "spawn":
+      var player = await getPlayer(request.playerID);
+
+      if (player && player.location && player.location.x && player.location.y) {
+        reject("Player has already spawned");
+        break;
+      }
+
+      var spawnLocation = [0,0];
+
+      updates["playerSecrets/" + request.playerID] = {
+        location: {
+          x: spawnLocation[0],
+          y: spawnLocation[1],
+        },
+      };
+
+      updates[`locations/${spawnLocation[0]}/${spawnLocation[1]}/object`] = {
+        type: "player",
+        playerID: request.playerID,
+        previousLocation: {
+          x: spawnLocation[0],
+          y: spawnLocation[1],
+        },
+      }
+      break;
+
+    // Move
+    case "move":
+      var player = await getPlayer(request.playerID);
+
+      if (!player || !player.location || !player.location.x || !player.location.y) {
+        reject("Player has not spawned?");
+        break;
+      }
+
+      var distance = distanceBetween([player.location.x, player.location.y], [request.target.x, request.target.y]);
+
+      if (distance > 6) {
+        reject("Distance too great");
+        break;
+      }
+
+      var currentLocation = await getLocation(player.location.x, player.location.y);
+      var targetLocation = await getLocation(request.target.x, request.target.y);
+
+      updates["playerSecrets/" + request.playerID] = {
+        location: {
+          x: request.target.x,
+          y: request.target.y,
+        },
+      };
+
+      updates[`locations/${player.location.x}/${player.location.y}/object`] = false;
+
+      updates[`locations/${request.target.x}/${request.target.y}/object`] = {
+        type: "player",
+        playerID: request.playerID,
+        previousLocation: {
+          x: player.location.x,
+          y: player.location.y,
+        },
+      }
+      break;
+
+    // Reject
+    default:
+      reject("Unknown action");
+  }
+
+  if (Object.keys(updates).length > 0) {
+    console.log("Successfully processed");
+    return database.ref().update(updates).then(resolve).catch(reject);
+  }
+  else {
+    console.log("No updates to make");
+    return reject("No updates to make");
+  }
+}
+
+var getPlayer = function(playerID) {
+  return database.ref("playerSecrets/" + playerID).once("value").then(function(snapshot) {
+    return snapshot.val();
+  });
+}
+
+var getLocation = function(x, y) {
+  return database.ref("locations/" + x + "/" + y).once("value").then(function(snapshot) {
+    return snapshot.val();
+  });
+}
+
+var distanceBetween = function(origin, target) {
+  return (
+    Math.abs(
+      Math.sqrt(
+          Math.pow(origin[0] - target[0], 2)
+        + Math.pow(origin[1] - target[1], 2)
+      )
+    )
+  );
+}
+
+// Player queue
+var playerQueue = new Queue(
+  database.ref("playerQueue"),
+  {"numWorkers": 5},
+  prepareRequest
+);
 
 // Game queue
-var actionQueue = new Queue(database.ref("actionQueue"), {"numWorkers": 5}, function(data, progress, resolve, reject) {
-  var request = data.request;
-  console.log("actionQueue:", request.action, "for", request.playerID, "in");
+var actionQueue = new Queue(
+  database.ref("actionQueue"),
+  {"numWorkers": 5},
+  prepareRequest
+);
 
-  if (request) {
-    var updates = {};
-
-    switch (request.action) {
-      default:
-        reject("Unknown action");
-    }
-  }
-  else {
-    reject("Missing request or request attributes");
-  }
-});
+console.log("Queues started");

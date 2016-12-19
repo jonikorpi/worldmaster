e9371b726570fff8c620bba55e89017308dd5153
Joni Korpi <me@jonikorpi.com>
2016-12-19 16:16:12 +0200

    Comments out move, probably mods spawn to work with new structure

--- queue.js
+++ queue.js
@@ -46,7 +46,6 @@ const processRequest = async function(request, progress, resolve, reject) {
         break;
       }
 
-
       let spawnFound = false;
       let spawnLocation;
 
@@ -56,122 +55,133 @@ const processRequest = async function(request, progress, resolve, reject) {
           Math.floor(Math.random() * 20) - 10,
         ];
 
-        if (await !fetchLocation(spawnLocation[0], spawnLocation[1]).tileOwner) {
+        if (await !fetchLocation(spawnLocation[0], spawnLocation[1]).units) {
           spawnFound = true;
         }
       }
 
+      const location = `locations/${spawnLocation[0]},${spawnLocation[1]}`;
+      const unitKey = database.ref().push().key;
+
+      // Update player index
       updates[`playerSecrets/${request.playerID}`] = {
         "message": "Successfully spawned",
-        "turn": 1,
         locations: {
-          [spawnLocation[0]]: {
-            [spawnLocation[1]]: true
-          }
+          [`${spawnLocation[0]},${spawnLocation[1]}`]: true
         },
       };
 
-      const location = `locations/${spawnLocation[0]}/${spawnLocation[1]}`;
+      // Add unit
+      updates[`${location}/units/${unitKey}`] = {
+        "x": 0,
+        "y": 0,
+        "playerID": request.playerID,
+        "type": "tower",
+        "actions": "1",
+        "action": "spawn",
+        "actionTime": Date.now(),
+        "emote": null,
+        "health": 30,
+        "orientation": 0,
+        "attack": "2 2 2 2 2 2",
+      };
 
-      updates[`${location}/tileOwner`] = request.playerID;
-      updates[`${location}/unit`] = "tower";
-      updates[`${location}/unitOwner`] = request.playerID;
-      updates[`${location}/unitLastX`] = null;
-      updates[`${location}/unitLastY`] = null;
-      updates[`${location}/unitLastTurn`] = 0;
-      updates[`${location}/unitLastAction`] = "spawn";
+      // Level tile
+      updates[`${location}/tiles/0,0`] = {
+        rock: 1,
+      };
 
       break;
 
     // Move
     case "move":
+      // //
+      // // Preliminary checks
       //
-      // Preliminary checks
-
-      // Is the distance short enough?
-      const distance = distanceBetween([request.origin.x, request.origin.y], [request.target.x, request.target.y]);
-
-      if (distance > 1.5) {
-        error("Distance too great", request.playerID, updates, reject);
-        break;
-      }
-
+      // // Is the distance short enough?
+      // const distance = distanceBetween([request.origin.x, request.origin.y], [request.target.x, request.target.y]);
       //
-      // Check origin and target
-
-      const [origin, target] = await Promise.all([
-        fetchLocation(request.origin.x, request.origin.y),
-        fetchLocation(request.target.x, request.target.y)
-      ]);
-
-
-      // Does origin contain your unit?
-      if (origin.unitOwner !== request.playerID) {
-        error("Can't move units you don't own", request.playerID, updates, reject);
-        break;
-      }
-
+      // if (distance > 1.5) {
+      //   error("Distance too great", request.playerID, updates, reject);
+      //   break;
+      // }
       //
-      // Try to perform the action on target
-
-      const target = referenceLocation(request.target.x, request.target.y);
-      let oldTargetValue;
-
-      let targetTransaction = await target.transaction(async function(targetValue) {
-        // Save first target value
-        if (!oldTargetValue) {
-          oldTargetValue = targetValue;
-        }
-
-        // Is target occupied?
-        if (targetValue.unit) {
-          error("Can't move onto another unit", request.playerID, updates, reject);
-          return;
-        }
-
-        else {
-          targetValue.tileOwner = request.playerID;
-          targetValue.unit = fetchedOrigin.unit;
-          targetValue.unitOwner = fetchedOrigin.unitOwner;
-          targetValue.unitLastX = request.origin.x;
-          targetValue.unitLastY = request.origin.y;
-          targetValue.unitLastTurn = fetchedOrigin.unitLastTurn;
-          targetValue.unitLastAction = "move";
-
-          return targetValue;
-        }
-      });
-
-      if (!targetTransaction.committed) {
-        error("Action on the target failed", request.playerID, updates, reject);
-        break;
-      }
-
+      // //
+      // // Check origin and target
       //
-      // Try to inflict the consequences on origin
-
-      const origin = referenceLocation(request.origin.x, request.origin.y);
-      const newTargetValue = targetTransaction.snapshot;
-
-      let originTransaction = await origin.transaction((originValue) => {
-        originValue.unit = null;
-        originValue.unitOwner = null;
-        originValue.unitLastX = null;
-        originValue.unitLastY = null;
-        originValue.unitLastTurn = null;
-        originValue.unitLastAction = null;
-
-        return originValue;
-      });
-
+      // const [origin, target] = await Promise.all([
+      //   fetchLocation(request.origin.x, request.origin.y),
+      //   fetchLocation(request.target.x, request.target.y)
+      // ]);
       //
-      // Update indexes
-
-      updates[`playerSecrets/${request.playerID}/locations/${request.target.x}/${request.target.y}`] = true;
-
-      if (oldTargetValue.tileOwner) {
-        updates[`playerSecrets/${target.tileOwner}/locations/${request.target.x}/${request.target.y}`] = null;
-      }
+      //
+      // // Does origin contain your unit?
+      // if (origin.unitOwner !== request.playerID) {
+      //   error("Can't move units you don't own", request.playerID, updates, reject);
+      //   break;
+      // }
+      //
+      // //
+      // // Try to perform the action on target
+      //
+      // const target = referenceLocation(request.target.x, request.target.y);
+      // let oldTargetValue;
+      //
+      // let targetTransaction = await target.transaction(async function(targetValue) {
+      //   // Save first target value
+      //   if (!oldTargetValue) {
+      //     oldTargetValue = targetValue;
+      //   }
+      //
+      //   // Is target occupied?
+      //   if (targetValue.unit) {
+      //     error("Can't move onto another unit", request.playerID, updates, reject);
+      //     return;
+      //   }
+      //
+      //   else {
+      //     targetValue.tileOwner = request.playerID;
+      //     targetValue.unit = fetchedOrigin.unit;
+      //     targetValue.unitOwner = fetchedOrigin.unitOwner;
+      //     targetValue.unitLastX = request.origin.x;
+      //     targetValue.unitLastY = request.origin.y;
+      //     targetValue.unitLastTurn = fetchedOrigin.unitLastTurn;
+      //     targetValue.unitLastAction = "move";
+      //
+      //     return targetValue;
+      //   }
+      // });
+      //
+      // if (!targetTransaction.committed) {
+      //   error("Action on the target failed", request.playerID, updates, reject);
+      //   break;
+      // }
+      //
+      // //
+      // // Try to inflict the consequences on origin
+      //
+      // const origin = referenceLocation(request.origin.x, request.origin.y);
+      // const newTargetValue = targetTransaction.snapshot;
+      //
+      // let originTransaction = await origin.transaction((originValue) => {
+      //   originValue.unit = null;
+      //   originValue.unitOwner = null;
+      //   originValue.unitLastX = null;
+      //   originValue.unitLastY = null;
+      //   originValue.unitLastTurn = null;
+      //   originValue.unitLastAction = null;
+      //
+      //   return originValue;
+      // });
+      //
+      // //
+      // // Update indexes
+      //
+      // updates[`playerSecrets/${request.playerID}/locations/x:${request.target.x},y:${request.target.y}`] = true;
+      //
+      // if (oldTargetValue.tileOwner) {
+      //   updates[`playerSecrets/${target.tileOwner}/locations/x:${request.target.x},y:${request.target.y}`] = null;
+      // }
 
       break;
 
@@ -181,12 +191,10 @@ const processRequest = async function(request, progress, resolve, reject) {
   }
 
   if (Object.keys(updates).length > 0) {
-    console.log("Successfully processed");
     return database.ref().update(updates).then(resolve).catch(reject);
   }
-  else {
-    reject("No updates to make");
-  }
+
+  console.log("Successfully processed");
 }
 
 //
@@ -196,24 +204,24 @@ const fetchPlayer = function(playerID) {
   return database.ref(`playerSecrets/${playerID}`).once("value").then(function(snapshot) {
     return snapshot.val();
   });
-}
+};
 
 const fetchLocation = function(x, y) {
-  return database.ref(`locations/${x}/${y}`).once("value").then(function(snapshot) {
+  return database.ref(`locations/${x},${y}`).once("value").then(function(snapshot) {
     return snapshot.val();
   });
-}
+};
 
 const referenceLocation = function(x, y) {
-  return database.ref(`locations/${x}/${y}`);
-}
+  return database.ref(`locations/${x},${y}`);
+};
 
 const error = function(message, playerID, reject) {
   console.log(playerID, message);
   let updates = {};
   updates[`playerSecrets/${playerID}/message`] = message;
   return database.ref().update(updates).then(reject).catch(reject);
-}
+};
 
 const distanceBetween = function(origin, target) {
   return (
@@ -224,6 +232,34 @@ const distanceBetween = function(origin, target) {
       )
     )
   );
+};
+
+const listNeighbouringTiles = function(location) {
+  const [x, y] = location;
+  let neighbours = [];
+
+  // Clockwise from top-right
+  neighbours.push([x+1, y-1]);
+  neighbours.push([x+1, y  ]);
+  neighbours.push([x,   y+1]);
+  neighbours.push([x-1, y+1]);
+  neighbours.push([x-1, y  ]);
+  neighbours.push([x,   y-1]);
+
+  return neighbours;
+};
+
+const listTilesInRange = function(location, range) {
+  const [x, y] = location;
+  let tiles = [];
+
+  for (let x = -range; x <= range; x++) {
+    for (let y = Math.max(-range, -x-range); y <= Math.min(range, -x+range); y++) {
+      tiles.push([x, y]);
+    }
+  }
+
+  return tiles;
 }
 
 //
